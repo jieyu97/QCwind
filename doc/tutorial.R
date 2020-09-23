3eeed98d870215f7fff8c59adb99daaf1f4f21ac
Jieyu Chen <Selena970703@hotmail.com>
2020-09-23 15:20:46 +0200

    check

---
+++ doc/tutorial.R
@@ -0,0 +1,8 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+## ----start, message=FALSE, include=FALSE--------------------------------------
+# library(QCwind)
+library(plotly)
+# load() # load date from this package
+
